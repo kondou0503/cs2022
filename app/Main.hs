3dbf1117cdac995d66025cdb98399e379242e386
近藤七海 <nanami@kondounanaminoMacBook-Pro-3.local>
2022-09-26 17:02:45 +0900

    stdout 1.2

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
-import Lib
-
 main :: IO ()
-main = someFunc
+main = interact func
+
+-- func :: String -> String
+-- func input = unlines ["Hello Algo-method!"]
+
+func :: String -> String
+func input = unlines (map showInt [1 .. 3])
+
+showInt :: Int -> String
+showInt = show
